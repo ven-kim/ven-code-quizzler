e7c74d2bd024e3055c533159bd1cefbca328d947
Ven Kim <venvex@gmail.com>
2021-07-15 23:08:17 -0400

    modified game.js for testing

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,59 @@
-const question = document.querySelector()
+/*
+const question = document.querySelector('#question');
+const choices = array.from(document.querySelectorAll('.choice-text'));
+const progressText = document.querySelector('#progressText');
+const scoreText = document.querySelector('#score');
+const Timer = document.querySelector('#timer');
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+let timer = 60
+
+let questions = [
+    {
+        question: 'What does HTML stand for?',
+        choice1: 'Hyper Text Making Language',
+        choice2: 'Hyper Text Markup Language',
+        choice3: 'Hyphen Text Marker Logistics',
+        choice4: 'Hope The Machine Launches',
+        answer: 'Hyper Text Markup Language',
+    },
+    {
+        question: 'What does CSS stand for?',
+        choice1: 'Collapsing Style Sheets',
+        choice2: 'Concave Standard Stove',
+        choice3: 'Cool Structured School',
+        choice4: 'Cascading Style Sheets',
+        answer: 'Cascading Style Sheets',
+    },
+    {
+        question: 'Which of these is not a git command?',
+        choice1: 'git commit',
+        choice2: 'git init',
+        choice3: 'git ineedagoodgrade',
+        choice4: 'git merge',
+        answer: 'git ineedagoodgrade',
+    },
+    {
+        question: 'How would you write "please" as a class element for a section?',
+        choice1: '<section class="please"></section>',
+        choice2: '<please class="section"></please>',
+        choice3: '<section id="please"></section>',
+        choice4: '<class="please" section></section>',
+        answer: '<section class="please"></section>',
+    },
+    {
+        question: 'Which of the following is not a CSS property?',
+        choice1: 'padding',
+        choice2: 'background-color',
+        choice3: 'margin',
+        choice4: 'soaboutthatgrade',
+        answer: 'soaboutthatgrade',
+    },
+]
+
+*/
+
